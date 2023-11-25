6e161a7d0050c789fffdf4629db6a5c75aeb585b
Lucas Fernandes <lucasfernandesprogrammer@gmail.com>
2023-11-24 23:33:03 -0300

    feat: tratamento de erros em envio de mensagem em PR

--- src/azure/app.service.ts
+++ src/azure/app.service.ts
@@ -9,27 +9,56 @@ export class AppService {
   }
 
   handlePullRequest(pullRequestDto: PullRequestDto): string {
-    fetch(`https://${process.env.URL_WEBHOOK_DISCORD}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        content: null,
-        embeds: [
-          {
-            title: 'Solicitação de CODE REVIEW',
-            description: `Repositório: ${pullRequestDto.resource.repository.name}\nBranch: ${pullRequestDto.resource.sourceRefName}\nAutor: ${pullRequestDto.resource.createdBy.displayName}\n\n${pullRequestDto.detailedMessage.markdown}`,
-            color: 776715,
-            author: { name: 'Pull Request criado' },
+    const webhookUrl = `https://${process.env.URL_WEBHOOK_DISCORD}`;
+    const maxRetries = 3;
+    let retryCount = 0;
+  
+    const sendRequest = async () => {
+      try {
+        const response = await fetch(webhookUrl, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
           },
-        ],
-        attachments: [],
-      }),
-    }).then((res) => {
-      return res.json();
-    });
-
-    return 'Ok';
+          body: JSON.stringify({
+            content: null,
+            embeds: [
+              {
+                title: 'Solicitação de CODE REVIEW',
+                description: `Repositório: ${pullRequestDto.resource.repository.name}\nBranch: ${pullRequestDto.resource.sourceRefName}\nAutor: ${pullRequestDto.resource.createdBy.displayName}\n\n${pullRequestDto.detailedMessage.markdown}`,
+                color: 776715,
+                author: { name: 'Pull Request criado' },
+              },
+            ],
+            attachments: [],
+          }),
+        });
+  
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+  
+        return response.json();
+      } catch (error) {
+        if (retryCount < maxRetries) {
+          retryCount++;
+          console.error(`Erro ao enviar a requisição. Tentativa ${retryCount} de ${maxRetries}.`);
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+          return sendRequest(); // Chama a função novamente
+        } else {
+          console.error(`Número máximo de tentativas atingido. Erro: ${error.message}`);
+          throw error;
+        }
+      }
+    };
+  
+    try {
+      sendRequest();
+      return 'Ok';
+    } catch (error) {
+      console.error(`Erro final: ${error.message}`);
+      return 'Erro';
+    }
   }
+  
 }
